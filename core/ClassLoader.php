8728f73b685c5153efb4a5859d8cee007553289f
今陸成 <rkon@corron.jp>
2016-12-19 18:18:37 +0900

    add some core classes

---
+++ core/ClassLoader.php
@@ -0,0 +1,23 @@
+<?php
+class ClassLoader {
+    protected $dirs;
+
+    public function register() {
+        spl_autoload_register(array($this, 'loadClass'));
+    }
+
+    public function registerDir($dir) {
+        $this->dirs[] = $dir;
+    }
+
+    public function loadClass($class) {
+        foreach ($this->dirs as $dir) {
+            $fiel = $dir . '/' . $class . '.php';
+            if ($is_readable($file)) {
+                require $file;
+
+                return;
+            }
+        }
+    }
+}
